84f5499351f92cf0f661a2212f5cee588a09a297
Justin Hudis <justin.hudis@gmail.com>
2023-12-07 23:14:49 -0500

    Add starter code

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstdlib>
+#include <random>
+
+int main() {
+    int d = 64;      // dimension
+    int nb = 100000; // database size
+    int nq = 10000;  // nb of queries
+
+    std::mt19937 rng;
+    std::uniform_real_distribution<> distrib;
+
+    float* xb = new float[d * nb];
+    float* xq = new float[d * nq];
+
+    for (int i = 0; i < nb; i++) {
+        for (int j = 0; j < d; j++)
+            xb[d * i + j] = distrib(rng);
+        xb[d * i] += i / 1000.;
+    }
+
+    for (int i = 0; i < nq; i++) {
+        for (int j = 0; j < d; j++)
+            xq[d * i + j] = distrib(rng);
+        xq[d * i] += i / 1000.;
+    }
+
+    return 0;
+}
